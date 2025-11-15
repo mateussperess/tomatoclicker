08eddeaf73b7b5c6ac1c2b51dac3ffef7db29219
mateussperess <mateuspereslopesl@outlook.com>
2025-11-15 12:04:03 -0300

    finalizando

--- app/src/main/java/com/example/atividade_jogo_clique/MainActivity.java
+++ app/src/main/java/com/example/atividade_jogo_clique/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.atividade_jogo_clique;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +16,15 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     ImageView ivFoto;
+    TextView txtTitle;
+    Button btnReset;
     int counter;
+    TextView txtHistory;
+
+    public static String PREFS = "prefs";
+    public static String CHAVE_CLIQUES = "cliques";
+    public static String CHAVE_IMG = "img";
+    int lastImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +33,70 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ivFoto = findViewById(R.id.tomato);
+        txtTitle = findViewById(R.id.gameTitle);
+        btnReset = findViewById(R.id.btnReset);
+        txtHistory = findViewById(R.id.gameTotalClicks);
+
         ivFoto.setOnClickListener(v -> {
-            Log.d("CLIQUE", "O tomate 1 foi clicado");
+//            Log.d("CLIQUE", "O tomate 1 foi clicado");
             counter++;
 
-            if(counter == 10) {
+            lastImg = getTomatoDrawable(Tomatoes.TOMATE1);
+
+            if(counter > 10) {
                 ivFoto.setImageResource(getTomatoDrawable(Tomatoes.TOMATE2));
+                lastImg = getTomatoDrawable(Tomatoes.TOMATE2);
             }
+
+            if (counter > 20) {
+                ivFoto.setImageResource(getTomatoDrawable(Tomatoes.TOMATE3));
+                lastImg = getTomatoDrawable(Tomatoes.TOMATE3);
+            }
+
+            if (counter > 30) {
+                ivFoto.setImageResource(getTomatoDrawable(Tomatoes.TOMATE4));
+                lastImg = getTomatoDrawable(Tomatoes.TOMATE4);
+            }
+            if (counter > 40) {
+                ivFoto.setImageResource(getTomatoDrawable(Tomatoes.TOMATE5));
+                lastImg = getTomatoDrawable(Tomatoes.TOMATE5);
+            }
+            if (counter > 50) {
+                txtTitle.setText("");
+                ivFoto.setImageResource(getTomatoDrawable(Tomatoes.TOMATE6));
+                lastImg = getTomatoDrawable(Tomatoes.TOMATE6);
+            }
+            if (counter > 60) {
+                ivFoto.setImageResource(getTomatoDrawable(Tomatoes.TOMATE7));
+                lastImg = getTomatoDrawable(Tomatoes.TOMATE7);
+            }
+            if (counter > 70) {
+                ivFoto.setImageResource(getTomatoDrawable(Tomatoes.TOMATE8));
+                lastImg = getTomatoDrawable(Tomatoes.TOMATE8);
+            }
+            if (counter > 80) {
+                ivFoto.setImageResource(getTomatoDrawable(Tomatoes.TOMATE9));
+                lastImg = getTomatoDrawable(Tomatoes.TOMATE9);
+            }
+            if (counter > 90) {
+                ivFoto.setImageResource(getTomatoDrawable(Tomatoes.TOMATE10));
+                lastImg = getTomatoDrawable(Tomatoes.TOMATE10);
+            }
+
+            txtHistory.setText("Você já tomatizou: " + counter + " cliques!");
+            save();
+        });
+
+        read();
+
+        btnReset.setOnClickListener(v -> {
+            Toast.makeText(this, "Resetando...", Toast.LENGTH_SHORT).show();
+            ivFoto.setImageResource(getTomatoDrawable(Tomatoes.TOMATE1));
+            lastImg = getTomatoDrawable(Tomatoes.TOMATE1);
+            counter = 0;
+            txtHistory.setText("Você já tomatizou: 0 cliques!");
+            txtTitle.setText("Tomato Clicker");
+            save();
         });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -51,5 +120,32 @@ public class MainActivity extends AppCompatActivity {
             case TOMATE10: return R.drawable.tomate10;
         }
         return R.drawable.tomate1;
+    }
+
+    private void save() {
+        SharedPreferences preferences = getSharedPreferences(PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        editor.putInt(CHAVE_CLIQUES, counter);
+        editor.putInt(CHAVE_IMG, lastImg);
+        editor.apply();
+//        Toast.makeText(this, "Salvou", Toast.LENGTH_LONG).show();
+    }
+
+    private void read() {
+        SharedPreferences preferences = getSharedPreferences(PREFS, MODE_PRIVATE);
+        int cliques = preferences.getInt(CHAVE_CLIQUES, 0);
+        int img = preferences.getInt(CHAVE_IMG, R.drawable.tomate1);
+
+        counter = cliques;
+
+        if(counter > 50) {
+            txtTitle.setText("");
+        }
+
+        lastImg = img;
+        ivFoto.setImageResource(img);
+
+        txtHistory.setText("Você já tomatizou: " + cliques + " cliques!");
     }
 }
